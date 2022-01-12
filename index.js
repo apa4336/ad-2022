bbf766956d8ed9dd1c39e15fc46867bcd8a130ba
apa4336 <apa4336@gmail.com>
2022-01-12 15:50:34 +0800

    v1.0

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
 // 註解 ctrl + /
-     alert('Hello world123')
+    alert('Hello world123')
     document.write("<h1>Hello world</h1>")
+/**
+ * dgsgs
+ * dsadasdf
+ * gsgdeds
+ */
+
+
+    /*
+    11111
+    2222
+    3333
+    */
