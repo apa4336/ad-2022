73d314b6243cb37dc8ce6c3b76577d5782ba617b
apa4336 <apa4336@gmail.com>
2022-01-18 17:25:07 +0800

    v1.3

---
+++ index3-1.js
@@ -0,0 +1,17 @@
+let scroe = 59;
+/*
+if(scroe >= 60){
+    console.log)"您的分數是及格的!")
+}
+*/
+if(scroe >= 90){
+    console.log("您的分數是A!");
+
+} else if(scroe >= 80){
+    console.log("您的分數是B!");
+} else if(scroe >= 70){
+    console.log("您的分數是C!");
+} else if(scroe >= 60){
+    console.log("您的分數是D!");
+
+}else{console.log("您的分數是明年再來!")}
