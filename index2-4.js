af557815f072bc601c5fa9a3d76456dbf9b3c884
apa4336 <apa4336@gmail.com>
2022-01-18 11:38:24 +0800

    v1.2

--- index2-4.js
+++ index2-4.js
@@ -1,2 +1,11 @@
-let myname = '鄭名翔';
-console.log(myName);
+// let myName = '鄭名翔';
+// console.log(myName);
+let myName ='小凡';
+myNAme ='123';
+
+const PI =3.14159;
+
+var age = 25;
+//console.log(age)
+age = 40;
+console.log(age)
