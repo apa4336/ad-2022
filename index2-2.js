0a7bbbb6605583f21c81385a36805985ffe0f843
apa4336 <apa4336@gmail.com>
2022-01-18 09:31:03 +0800

    v1.1

---
+++ index2-2.js
@@ -0,0 +1,5 @@
+// console.log('123');
+// console.log("鄭名翔");
+// console.log(`123`);
+// alert("123");
+// prompt("請輸入您的名字");
---
+++ index2-3.js
@@ -0,0 +1,17 @@
+//  x = 5
+// let x;
+// console.log(x);
+// x = 5;
+// console.log(x);
+// x = '鄭名翔';
+// console.log(x);
+
+//let x = 5;
+// x = 6
+//console.log(x);
+
+// const PI =3.14159;
+// console.log(PI);
+
+x = 5;
+ console.log(x);
