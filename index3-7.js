5a715bb2d0b40007f3c90611c22a135a63c3567c
apa4336 <apa4336@gmail.com>
2022-11-19 16:41:43 +0800

    v1.2

--- index3-7.js
+++ index3-7.js
@@ -8,11 +8,11 @@ let rows = ['a', 'b', 'c'];
 // expected output: "a"
 // expected output: "b"
 // expected output: "c"
-let card1 = ['郭俊良', '0921560420', '台南市中西區西門路一段20號', '工程師']
+// let card1 = ['郭俊良', '0921560420', '台南市中西區西門路一段20號', '工程師']
 
-for(let i in card1){
-    console.log(card1[i]);
-}
+// for(let i in card1){
+//     console.log(card1[i]);
+// }
 
 
 // let card = {
