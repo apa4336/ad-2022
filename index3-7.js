9c1feffc009fff06f6be67f5b1228448f768db4e
apa4336 <apa4336@gmail.com>
2022-11-24 23:12:52 +0800

    v1.4

--- index3-7.js
+++ index3-7.js
@@ -8,20 +8,24 @@ let rows = ['a', 'b', 'c'];
 // expected output: "a"
 // expected output: "b"
 // expected output: "c"
+
 // let card1 = ['郭俊良', '0921560420', '台南市中西區西門路一段20號', '工程師']
 
-// for(let i in card1){
-//     console.log(card1[i]);
-// }
-
-
-// let card = {
-//     name: '郭俊良',
-//     tel: '0921560420',
-//     addr: '台南市中西區西門路一段20號',
-//     '職稱': '工程師'
-// };
-
+// for of 只能用在陣列
 // for(let value of card1){
 //     console.log(value);
 // }
+
+
+
+let card = {
+    name: '郭俊良',
+    tel: '0921560420',
+    addr: '台南市中西區西門路一段20號',
+    '職稱': '工程師'
+};
+
+// for in 陣列或物件都可以做
+for(let i in card){
+    console.log(card[i]);
+}
